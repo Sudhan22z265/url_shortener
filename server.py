1618ff2319e8c9e2cf9e2c9a200a799a4ad7c8c0
Your Name <you@example.com>
2024-01-25 13:06:40 +0530

    redirect added

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,redirect,url_for
 
 app = Flask(__name__)
 
@@ -8,9 +8,12 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/your-url')
+@app.route('/your-url',methods=['GET','POST'])
 def about():
-    return render_template('your_url.html',code=request.args['code'])
+    if request.method == 'POST':
+        return render_template('your_url.html',code=request.form['code'])
+    else:
+        return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
